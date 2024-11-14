a00bd2334d1b56ba1094628db2a17baf6a9566c8
FabianKjellberg <Fabian.Kjellberg98@gmail.com>
2024-11-14 23:26:20 +0100

    update port to use the port assigned by azure

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ async function initializeServer(){
 
     // Start the server
     const app = express();
-    const port = 3000;
+    const port = process.env.PORT || 3000;
 
     // Import routes
     const loginRoutes = require('./routes/loginRoutes');
