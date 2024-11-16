f74b0a29169c37852e8f508323353b05491d24b5
FabianKjellberg <Fabian.Kjellberg98@gmail.com>
2024-11-16 18:24:03 +0100

    redeploy

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { initializeSecrets } = require('./config/secretsConfig');
 async function initializeServer(){
     await initializeSecrets();
 
+    
     // Start the server
     const app = express();
     const port = process.env.PORT || 3000;
